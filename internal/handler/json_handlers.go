fead1ac701d209b86aded8693e1fb6a6c0a7a2e0
devize-ed <devivze.2@gmail.com>
2025-08-03 12:11:08 +0200

    handler fix

--- internal/handler/json_handlers.go
+++ internal/handler/json_handlers.go
@@ -120,11 +120,10 @@ func (h *Handler) GetMetricJSONHandler() http.HandlerFunc {
 
 		// Write response.
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 		if err := json.NewEncoder(w).Encode(body); err != nil {
 			logger.Log.Debug("Cannot encode response JSON:", err)
-			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		w.WriteHeader(http.StatusOK)
 	}
 }
